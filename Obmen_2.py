8944f493f9dc3683b342f80f78bbe40567cf351d
Oleg-192 <starter-nsk@yande[.ru>
2025-08-05 16:08:13 +0700

    Вместо поля ввода добавлен выпадающий список.

--- Obmen_2.py
+++ Obmen_2.py
@@ -1,23 +1,21 @@
 import requests
 import json
 from tkinter import *
-from tkinter import ttk
 from tkinter import messagebox as mb
 import pprint
 
 
 
-
 def exchange():
-    code = combobox.get()
+    code = entry.get()
     if code:
         try:
             response = requests.get('https://open.er-api.com/v6/latest/USD')
             response.raise_for_status()
             data = response.json()
-            p = pprint.PrettyPrinter(indent=4)
+            # p = pprint.PrettyPrinter(indent=4)
             # p.pprint(data)
-            p.pprint(data['rates'])
+            # p.pprint(data['rates'])
             # p.pprint(data['rates']['RUB'])
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
@@ -34,18 +32,11 @@ window = Tk()
 window.title('Курсы валют')
 window.geometry('360x180')
 
-Label(text='Выберите код валюты').pack(padx=10, pady=10)
+Label(text='введитке код валюты').pack(padx=10, pady=10)
 
-cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'AED', 'CAD' ]
-combobox = ttk.Combobox(values=cur)
-combobox.pack(padx=10, pady=10)
-
-# entry = Entry()
-# entry.pack(padx=10, pady=10)
+entry = Entry()
+entry.pack(padx=10, pady=10)
 
 Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
-
 window.mainloop()
-
-
