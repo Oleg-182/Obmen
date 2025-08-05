dfce7d6dcd29bcdf02d3f0217e2a68b548df76cf
Oleg-192 <starter-nsk@yande[.ru>
2025-08-05 17:00:16 +0700

    В метку добавлены названия валют.

--- Obmen_.py
+++ Obmen_.py
@@ -15,13 +15,14 @@ def exchange():
             response = requests.get('https://open.er-api.com/v6/latest/USD')
             response.raise_for_status()
             data = response.json()
-            p = pprint.PrettyPrinter(indent=4)
+            # p = pprint.PrettyPrinter(indent=4)
             # p.pprint(data)
-            p.pprint(data['rates'])
+            # p.pprint(data['rates'])
             # p.pprint(data['rates']['RUB'])
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo('Курс обмена', f'Курс: {exchange_rate:.2f} {code} за 1 доллар')
+                c_name = cur[code]
+                mb.showinfo('Курс обмена', f'Курс: {exchange_rate:.2f} {c_name} за 1 доллар')
             else:
                 mb.showerror('Ошибка', f' Валюта {code} не найдена.')
         except Exception as e:
@@ -30,18 +31,37 @@ def exchange():
         mb.showwarning('Внимание', 'Введите код валюты')
 
 
+def update_c_label(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
+
+
+cur = {
+    'RUB': 'Российский рубль',
+    'EUR': 'Евро',
+    'GBP': 'Британский фунт стерлингов',
+    'JPY': 'Японская йена',
+    'CNY': 'Китайский юань',
+    'KZT': 'Казахский тенге',
+    'UZS': 'Узбекский сум',
+    'AED': 'Дирхам ОАЭ',
+    'CAD': 'Канадский доллар'
+}
+
 window = Tk()
 window.title('Курсы валют')
 window.geometry('360x180')
 
 Label(text='Выберите код валюты').pack(padx=10, pady=10)
 
-cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'AED', 'CAD' ]
-combobox = ttk.Combobox(values=cur)
-combobox.pack(padx=10, pady=10)
 
-# entry = Entry()
-# entry.pack(padx=10, pady=10)
+combobox = ttk.Combobox(values=list(cur.keys()))
+combobox.pack(padx=10, pady=10)
+combobox.bind('<<ComboboxSelected>>', update_c_label)
+
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
 
 Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
